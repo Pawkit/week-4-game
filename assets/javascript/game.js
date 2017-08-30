1c3ce48161a1a01a8cb9ac7485558f66a4d4e8c3
Pawkit <patrickawong@gmail.com>
2017-08-29 18:15:42 -0700

    styling+update

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// GLOBAL VARIABLES
+// Arrays and variables for holding data
+// ========================================================================
+
+var redCrystal = 0
+var blueCrystal = 0
+var greenCrystal = 0
+var orangeCrystal = 0
+var wins = 0
+var losses = 0
+var targetNum = 0
+var userNum = 0
+
+// FUNCTIONS
+// ========================================================================
+$(".red-crystal").click(function() {
+	console.log("yay")
+	var random = Math.floor(Math.random()
+});
